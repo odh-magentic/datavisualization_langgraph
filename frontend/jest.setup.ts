d838a66b05f92424abcdccd2b79b2f16a9dcadca
odh-magentic <odh@magentic.com>
2025-01-03 22:52:46 +0000

    deleted unused files

--- frontend/jest.setup.ts
+++
@@ -1,5 +0,0 @@
-import '@testing-library/jest-dom/jest-globals'
-
-// Allow router mocks.
-// eslint-disable-next-line no-undef, global-require
-jest.mock('next/router', () => require('next-router-mock'))
--- frontend/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  poweredByHeader: false,
-  generateEtags: false,
-  reactStrictMode: true,
-  swcMinify: true,
-}
